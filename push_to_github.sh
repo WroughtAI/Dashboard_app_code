512116ae351c3d8aa9aa33059f626cd52ce01431
WroughtAI <jim@barnebee.com>
2025-06-04 19:20:13 -0400

    Update push script to point to new Dashboard_app_code repository

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git remote remove origin
-git remote add origin https://github.com/WroughtAI/Code_Attachement_to_scaffold.git
+git remote add origin https://github.com/WroughtAI/Dashboard_app_code.git
 git branch -M main
 git push -u origin main 
